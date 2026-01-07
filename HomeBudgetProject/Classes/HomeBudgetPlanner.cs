044ce8efce72a1517226238e68e165a9b912f74a
Bartek-Orl <bartosz.orl@wp.pl>
2026-01-07 20:04:56 +0100

    dodanie zarządzania wydatkami i przychodami

--- HomeBudgetProject/Classes/HomeBudgetPlanner.cs
+++ HomeBudgetProject/Classes/HomeBudgetPlanner.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HomeBudgetProject.Enums;
 using HomeBudgetProject.Interfaces;
 
 namespace HomeBudgetProject.Classes
@@ -12,14 +13,47 @@ namespace HomeBudgetProject.Classes
         private List<IBudgetObserver> observers = new List<IBudgetObserver>();
         public List<BudgetItem> budgetItemsList = new List<BudgetItem>();
         public IRaportStrategy raportStrategy;
+        private BudgetGroup expensesGroup;
+        private BudgetGroup incomeGroup;
+
+        public HomeBudgetPlanner()
+        {
+            incomeGroup = new BudgetGroup(Enums.CategoryType.Przychody);
+            expensesGroup = new BudgetGroup(Enums.CategoryType.WszystkieWydatki);
+
+            foreach (CategoryType category in Enum.GetValues(typeof(CategoryType)))
+            {
+                if (category == CategoryType.Przychody || category == CategoryType.WszystkieWydatki) continue;
+
+                BudgetGroup group = new BudgetGroup(category);
+                expensesGroup.Add(group);
+            }
+
+            budgetItemsList.Add(incomeGroup);
+            budgetItemsList.Add(expensesGroup);
+        }
         public void AddExpense(Expense item)
         {
-            throw new NotImplementedException();
+            BudgetGroup pomGroup = null;
+
+            foreach (BudgetGroup element in expensesGroup.budgetItemList)
+            {
+                if (item.Category == element.Category)
+                {
+                    pomGroup = element;
+                    break;
+                }
+            }      
+                pomGroup.Add(item);
+                Logger.GetInstance().Log($"Dodano wydatek: {item.Name}.");
+                Notify();
         }
 
         public void AddIncome(Income item)
         {
-            throw new NotImplementedException();
+            incomeGroup.Add(item);
+            Logger.GetInstance().Log($"Dodano przychód.");
+            Notify();
         }
 
         public void Attach(IBudgetObserver observer)
